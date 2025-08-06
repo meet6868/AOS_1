10c8161ea8808b3225343a13acbc8a0da85f9124
meet6868 <ghelanimeet2002@gmail.com>
2025-08-07 00:20:19 +0530

    Develeoped all three typr of flag funtion in normal mode, error handling remain

--- 2025202011_A1/2025202011_A1_Q1.cpp
+++ 2025202011_A1/2025202011_A1_Q1.cpp
@@ -6,10 +6,17 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <libgen.h>
-#include <string.h>
 #include <errno.h>
 #include <stdio.h>
 
+void copy_string(char* new_s, const char* old_S, int max_len=4048) {
+    int i = 0;
+    while (i < max_len - 1 && old_S[i] != '\0') {
+        new_s[i] = old_S[i];
+        i++;
+    }
+    new_s[i] = '\0'; 
+}
 
 void consol_out(const char* msg,bool is_Error=false){
     if(is_Error){
@@ -30,11 +37,11 @@ void consol_out(const char* msg,bool is_Error=false){
 
 void check_directory_permision(const char* file_path){
     char dir_path[PATH_MAX];
-    strncpy(dir_path, file_path, PATH_MAX);  
+    copy_string(dir_path, file_path, PATH_MAX);  
     char* dir_name = dirname(dir_path);   
 
     struct stat dir_status;
-    if (::stat(dir_name, &dir_status) == -1) {
+    if (stat(dir_name, &dir_status) == -1) {
         consol_out("Directory does not exist or cannot be accessed.", true);
     }
 
@@ -111,13 +118,13 @@ void flag_validation(const char* flag_arg, int argc){
 void extra_argument_validation(int flag,char* argv[]){
     if (flag == 0) {
         if (!check_is_number(argv[3])) {
-            consol_out("Please enter block size in numeric", true);
+            consol_out("Please enter block size in positive numeric", true);
         }
     }
 
     if (flag == 2) {
         if (!check_is_number(argv[3]) || !check_is_number(argv[4])) {
-            consol_out("Please enter numeric values for start and end index", true);
+            consol_out("Please enter positive numeric values for start and end index", true);
         }
         else{
             int start_index=atoi(argv[3]);
@@ -130,33 +137,46 @@ void extra_argument_validation(int flag,char* argv[]){
     consol_out("Extra Argument Validation checked");
 }
 
-void safe_path_concat(char* new_path, const char* base_path, const char* extra_path) {
-    
-    strcpy(new_path, base_path);
-    if (new_path[strlen(new_path) - 1] != '/') {
-        strcat(new_path, "/");
+void flag_2_extra_argument_validation(int start,int end,const char* file_name){
+    if(start<0){
+        consol_out("Start index must be grater than 0 !!1",true);
     }
-
-    strcat(new_path, extra_path);
-}
-
-const char* generate_output_file(const char* input_path, int flag) {
-    char* pathcopy1 = strdup(input_path);
-    char* pathcopy2 = strdup(input_path);
-    if (!pathcopy1 || !pathcopy2) {
-        perror("strdup failed");
+    int fd_in = open(file_name, O_RDONLY);
+    if(fd_in < 0){
+        consol_out("Error opening file to validate end index", true);
         _exit(1);
     }
 
-    const char* base_dir = dirname(pathcopy1);
-    const char* base_file = basename(pathcopy2); 
+    off_t file_size = lseek(fd_in, 0, SEEK_END);
+    close(fd_in);
+
+    if(end > file_size){
+        consol_out("End index exceeds file length!", true);
+    }
+
+    if(start >= end){
+        consol_out("Start index must be less than End index!", true);
+    }
+    consol_out("Flag 2 extra argument validation Checked");
+}
+
+const char* generate_output_file(const char* input_path, int flag) {
+    char* path= (char*)malloc(strlen(input_path) + 1);
+    if (!path) {
+        perror("malloc failed");
+        _exit(1);
+    }
+    copy_string(path,input_path);
+
+    const char* base_dir = dirname(path);
+    const char* base_file = basename(path); 
     char ass_dir[1024];
     snprintf(ass_dir, sizeof(ass_dir), "%s/Assignment1", base_dir);
 
     struct stat st;
     if (stat(ass_dir, &st) == -1) {
         if (mkdir(ass_dir, 0700) == -1) {
-            perror("mkdir failed");
+            perror("Mkdir failed"); 
             _exit(1);
         }
     } else if (!S_ISDIR(st.st_mode)) {
@@ -166,14 +186,14 @@ const char* generate_output_file(const char* input_path, int flag) {
 
 
     char file_name[512];
-    snprintf(file_name, sizeof(file_name), "%d_%s.txt", flag, base_file);
+    snprintf(file_name, sizeof(file_name), "%d_%s", flag, base_file);
 
     char full_path[2048];
     snprintf(full_path, sizeof(full_path), "%s/%s", ass_dir, file_name);
 
     int fd = open(full_path, O_CREAT | O_WRONLY | O_TRUNC, 0600);
     if (fd == -1) {
-        perror("file creation failed");
+        consol_out("File creation failed",true);
         _exit(1);
     }
     close(fd);
@@ -183,11 +203,8 @@ const char* generate_output_file(const char* input_path, int flag) {
         perror("malloc failed");
         _exit(1);
     }
-    strcpy(result, full_path);
-
-    free(pathcopy1);
-    free(pathcopy2);
-
+    copy_string(result, full_path);
+   
     return result;
 }
 
@@ -197,6 +214,25 @@ void reverse_block(char* block, int length) {
         block[i] = block[length - i - 1];
         block[length - i - 1] = temp;
     }
+}
+
+void update_progress_bar(int curr, int n) {
+    float per = (float)curr / n;
+    int bar_width = 30;
+
+    char buff[100];
+    int len = snprintf(buff, sizeof(buff), "\r[Block %d/%d] [", curr, n);
+    
+    int prog_str = (int)(per * bar_width);
+    for (int i = 0; i < bar_width; ++i) {
+        buff[len++] = (i < prog_str) ? '=' : ' ';
+    }
+    buff[len++] = ']';
+
+    len += snprintf(buff + len, sizeof(buff) - len, " %3.0f%%", per * 100);
+
+    write(STDOUT_FILENO, buff, len);
+    fsync(STDOUT_FILENO);
 }
 
 void flag_0_operation(const char* input_file, const char* output_file, const char* BLOCK_SIZE) {
@@ -222,10 +258,11 @@ void flag_0_operation(const char* input_file, const char* output_file, const cha
         read(fd_in, block, bytes_to_read);
         current_block++;
         if (current_block > total_blocks) current_block = total_blocks;
+        update_progress_bar(current_block, total_blocks);
 
         reverse_block(block, bytes_to_read);
-
         write(fd_out, block, bytes_to_read);
+        sleep(1);
     }
 
     delete[] block;
@@ -233,8 +270,86 @@ void flag_0_operation(const char* input_file, const char* output_file, const cha
     close(fd_out);
 }
 
+void flag_1_operation(const char* input_file, const char* output_file){
+    int fd_in = open(input_file, O_RDONLY);
+    int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
+    off_t file_size = lseek(fd_in, 0, SEEK_END);
+    off_t offset = file_size;
+    int block_size = 256;
+    char* block = new char[block_size];
 
+    int total_blocks = (file_size + block_size - 1) / block_size;
+    int current_block = 0;
+
+    while (offset > 0) {
+        int bytes_to_read = (offset >= block_size) ? block_size : offset;
+        offset -= bytes_to_read;
+        lseek(fd_in, offset, SEEK_SET);
+        read(fd_in, block, bytes_to_read);
+        current_block++;
+        if (current_block > total_blocks) current_block = total_blocks;
+        update_progress_bar(current_block, total_blocks);
+        
+        reverse_block(block, bytes_to_read);
+        write(fd_out, block, bytes_to_read);
+        sleep(1);
+    }
+
+    delete[] block;
+    close(fd_in);
+    close(fd_out);
+}
+
+void flag_2_operation(const char* input_file, const char* output_file,int start, int end){
+    int fd_in = open(input_file, O_RDONLY);
+    int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    off_t file_size = lseek(fd_in, 0, SEEK_END);
+
+    int block_size = 256;
+    char* block = new char[block_size];
+    lseek(fd_in, 0, SEEK_SET);
+    off_t s = 0;
+    while (s < start) {
+        int bytes_to_read = (start - s >= block_size) ? block_size : (start - s);
+        read(fd_in, block, bytes_to_read);
+        write(fd_out, block, bytes_to_read);
+        s += bytes_to_read;
+    }
+
+    
+    off_t offset = end;
+    int total_bytes = end - start;
+    int total_blocks = (total_bytes + block_size - 1) / block_size;
+    int current_block = 0;
+
+    while (offset > start) {
+        int bytes_to_read = (offset - start >= block_size) ? block_size : (offset - start);
+        offset -= bytes_to_read;
+        lseek(fd_in, offset, SEEK_SET);
+        read(fd_in, block, bytes_to_read);
+        current_block++;
+        if (current_block > total_blocks) current_block = total_blocks;
+        update_progress_bar(current_block, total_blocks);
+
+        reverse_block(block, bytes_to_read);
+        write(fd_out, block, bytes_to_read);
+        sleep(1);
+    }
+
+    s = end;
+    lseek(fd_in, s, SEEK_SET);
+    while (s < file_size) {
+        int bytes_to_read = (file_size - s >= block_size) ? block_size : (file_size - s);
+        read(fd_in, block, bytes_to_read);
+        write(fd_out, block, bytes_to_read);
+        s += bytes_to_read;
+    }
+
+    delete[] block;
+    close(fd_in);
+    close(fd_out);
+}
 int main(int argc ,char* argv[]){
     if(argc<3 ||  argc>5){
         consol_out("Please, Enter corrcet number of arguments !!!",true);
@@ -246,7 +361,6 @@ int main(int argc ,char* argv[]){
 
     const char* input_file=argv[1];
     
-
     //validations
     file_validation_check(input_file);
     flag_validation(argv[2],argc);
@@ -257,11 +371,18 @@ int main(int argc ,char* argv[]){
     if(flag==0){
         flag_0_operation(input_file,output_file,argv[3]);
     }
+    else if(flag==1){
+        flag_1_operation(input_file,output_file);
+    }
+    else if(flag==2){
+        int start=atoi(argv[3]);
+        int end=atoi(argv[4]);
+        flag_2_extra_argument_validation(start,end,input_file); 
+        flag_2_operation(input_file,output_file,start,end);
+    }
 
-    
-    
-    consol_out("You validated files Checks");
-
+    consol_out("\nYou File Generated at Output file at");
+    consol_out(output_file);
 
 
     return 0;
